3351d9e8b1ca0e424ab317716ae8209c2f59c395
Morning <morning@mtars-lab2-17s-iMac.local>
2023-01-02 12:39:19 +0800

    products

--- my-react-app-module/src/App.js
+++ my-react-app-module/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import logo from './products/image-yellow.png';
+import logoAqua from './products/image-aqua.png';
+// import logoRose from './products/image-rose.png';
+// import logoImage from './products/image.png';
+import logoSteel from './products/image-steel.png';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Popular Products<hr/>
+        </h1>
       </header>
+      <div class="container">
+      <div class="section1">
+        <img src={logo}></img>
+      </div>
+      <div class="section2">
+        <img src={logoAqua}></img>
+      </div>
+      <div class="section3">
+        <img src={logoSteel}></img>
+      </div>
+      </div>
+
     </div>
   );
 }
