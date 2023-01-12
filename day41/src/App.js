f3d80330aee8dcf28ea94cb1cd1ff5c6693918bb
Morning <morning@mtars-lab2-17s-iMac.local>
2023-01-12 09:44:38 +0800

    day-41

---
+++ day41/src/App.js
@@ -0,0 +1,39 @@
+
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [counter, setCounter] = useState(0)
+  const [inputText, setInputText] = useState("")
+
+
+  function handleClick() {
+    console.log("hello");
+  }
+
+  function handleInput(event) {
+    console.log(event.target.value);
+    setInputText(event.target.value)
+
+  }
+
+
+  return (
+    <div className="App">
+      <div id="ex-1">
+        <p>Hello day-41 state</p>
+        <button onClick={() => {
+          handleClick()
+          setCounter(counter + 1)
+        }} >Click me {counter}
+        </button>
+      </div>
+      <div id="ex-2">
+        <p>{inputText}</p>
+        <input value={inputText} onChange={ (e) => {handleInput(e)}}></input>
+      </div>
+    </div>
+  );
+}
+
+export default App;
