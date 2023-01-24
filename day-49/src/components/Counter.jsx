8c767f5004bdb4eaca1e90e908e950e3395eb8bf
Morning <morning@mtars-lab2-17s-iMac.local>
2023-01-24 12:49:39 +0800

    day-49

---
+++ day-49/src/components/Counter.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+export default function Counter({ isFancy, setIsFancy }) {
+  const [score, setScore] = useState(0);
+  const [hover, setHover] = useState(false);
+  let className = "counter";
+  if (hover) {
+    className += "hover";
+  }
+  if (isFancy) {
+    className += "fancy";
+  }
+
+  return (
+    <div>
+      <div
+        className={className}
+        onPointerEnter={() => {
+          setHover(true);
+        }}
+        onPointerLeave={() => {
+          setHover(false);
+        }}
+      >
+        <div>{score}</div>
+        <button
+          onClick={() => {
+            setScore(score + 1);
+          }}
+        >
+          Add one
+        </button>
+      </div>
+    </div>
+  );
+}
