89f86c91c7b63f996c432a086a2720c033900037
OtgonmunkhG <g.otgonmunkh@gmail.com>
2023-01-25 06:15:18 +0800

    try to onstop function

--- day-49/src/App.js
+++ day-49/src/App.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import "./App.css";
-import Timer from "./timercompenonts/Timer";
 import TimerDashboard from "./timercompenonts/TimerDashboard";
 
 function App() {
