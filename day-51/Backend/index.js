103e9f351abc95e298c6b57021bd975bf5a3f7d6
Morning <morning@mtars-lab2-17s-iMac.local>
2023-01-26 12:39:25 +0800

    added database with backend

---
+++ day-51/Backend/index.js
@@ -0,0 +1,52 @@
+console.log("day-51 : API Express js");
+
+const { response } = require("express");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const PORT = 8080;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+let data = [
+  {
+    id: 1,
+    name: "Otgoo",
+    major: "computer science",
+  },
+];
+app.get("/", (request, response) => {
+  response.send("Here im a brutally powerfull express server");
+});
+
+app.get("/data", (request, response) => {
+  response.json(data);
+});
+
+app.post("/data", (request, response) => {
+  //   console.log(request.headers);
+  console.log(request.body);
+  const length = data.length;
+  const newData = {
+    id: length + 1,
+    name: request.body.name,
+    major: request.body.major,
+  };
+  response.json(data);
+  data.push(newData);
+});
+app.delete("/data", (request, response) => {
+  console.log(request.body);
+  const newData = data.filter((d) => d.id !== request.body.id);
+  data = newData;
+  console.log(data);
+  response.json(data);
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on http://localhost:${PORT}`);
+});
