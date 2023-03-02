19a4d6350b058fd36a7da590acaf77336ccdfc37
Morning <morning@mtars-lab2-17s-iMac.local>
2023-03-02 12:16:22 +0800

    day-69 database add with crud & backend

---
+++ day-69-database/app.js
@@ -0,0 +1,19 @@
+console.log("DAY-69 DATABASE");
+
+import express, { response, Router } from "express";
+import emp_router from "./rootes/employees.js";
+
+const app = express();
+const PORT = 8080;
+app.use(express.json());
+app.use(emp_router);
+
+app.get("/", (request, response) => {
+  response.send(
+    "<h1 style='text-align:center; background-color: orange'>Day -69</h1>"
+  );
+});
+
+app.listen(PORT, () => {
+  console.log(`EXPRESS app is running on http://localhost:${PORT}`);
+});
