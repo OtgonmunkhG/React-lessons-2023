a91a814d9ae0ff490aae140579d1d45948848dc7
OtgonmunkhG <g.otgonmunkh@gmail.com>
2023-03-12 22:14:01 +0800

    update

--- day-69-msql-crud-employees/services/product-category.js
+++
@@ -1,6 +0,0 @@
-import { pool } from "../config/mysql-config.js";
-
-export async function getMaxNo() {
-  const [rows] = await pool.query("select max(emp_no) as max from employees");
-  return rows[0];
-}
