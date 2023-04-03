f7ee46bd03192928b439a589d5fae4d6d0754a0f
OtgonmunkhG <g.otgonmunkh@gmail.com>
2023-04-03 12:52:08 +0800

    prettier format

--- day-90-express-typescript/be/index.ts
+++ day-90-express-typescript/be/index.ts
@@ -1,14 +1,17 @@
-import express, {Request, Response} from "express"
+import express, { Request, Response } from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv"
-import theaterRouter from "./routes/Theaters.api"
-dotenv.config()
+import dotenv from "dotenv";
+import theaterRouter from "./routes/Theaters.api";
+dotenv.config();
 // import cors from "cors"
+
 const app = express();
 const PORT = process.env.PORT;
-const MONGO_CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://localhost:27017/test";
 
-let name:string = "<h1>Day - 90 Express-Typescript</h1>";
+const MONGO_CONNECTION_STRING =
+  process.env.MONGO_CONNECTION_STRING || "mongodb://localhost:27017/test";
+
+let name: string = "<h1>Day - 90 Express-Typescript</h1>";
 // name = 12;
 // let phoneNumber:number = 9999999;
 // let isMarried:boolean = false
@@ -29,7 +32,7 @@ let name:string = "<h1>Day - 90 Express-Typescript</h1>";
 
 app.use(express());
 // app.use(cors());
-app.use("/theaters", theaterRouter)
+app.use("/theaters", theaterRouter);
 
 app.get("/", (req: Request, res: Response) => {
   res.send(name);
