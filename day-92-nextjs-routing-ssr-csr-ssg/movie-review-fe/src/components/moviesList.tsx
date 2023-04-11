547a6cf30e97ececa4c4f9f13086b8dfb64faf65
OtgonmunkhG <g.otgonmunkh@gmail.com>
2023-04-11 11:03:10 +0800

    correct structer

--- day-92-nextjs-routing-ssr-csr-ssg/movie-review-fe/src/components/moviesList.tsx
+++ day-92-nextjs-routing-ssr-csr-ssg/movie-review-fe/src/components/moviesList.tsx
@@ -76,50 +76,51 @@ export default function MoviesList(): JSX.Element {
     fetchedAll(URL);
   }, []);
   return (
-    <div className=" text-black grid md:grid-cols-6 gap-3 sm:grid-cols-2 justify-between container mx-auto lg:max-w-6xl">
-      {movie.map((element, index) =>
-        element.poster ? (
-          <div key={index} className="mx-auto px-1	text-center">
-            <img
-              className="hover:opacity-75 rounded-lg h-96  shadow-xl w-72 md:h-60"
-              src={element.poster}
-              alt=""
-            />
-            <div className="text-black grid md:grid-cols-4 gap-3  sm:grid-cols-4 justify-between container mx-auto">
-              <div className="grid md:grid-cols-2 sm:grid-cols-2">
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Rotten_Tomatoes.svg/278px-Rotten_Tomatoes.svg.png?20180311145654"
-                  alt=""
-                  className="h-5 w-5"
-                />
-                <span>{element.tomatoes.viewer.rating}</span>
+    <div>
+      <div className=" text-black grid md:grid-cols-6 gap-3 sm:grid-cols-2 justify-between container mx-auto lg:max-w-6xl">
+        {movie.map((element, index) =>
+          element.poster ? (
+            <div key={index} className="mx-auto px-1	text-center">
+              <img
+                className="hover:opacity-75 rounded-lg h-96  shadow-xl w-72 md:h-60"
+                src={element.poster}
+                alt=""
+              />
+              <div className="text-black grid md:grid-cols-4 gap-3  sm:grid-cols-4 justify-between container mx-auto">
+                <div className="grid md:grid-cols-2 sm:grid-cols-2">
+                  <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Rotten_Tomatoes.svg/278px-Rotten_Tomatoes.svg.png?20180311145654"
+                    alt=""
+                    className="h-5 w-5"
+                  />
+                  <span>{element.tomatoes.viewer.rating}</span>
+                </div>
+                <div className="grid md:grid-cols-2 sm:grid-cols-2">
+                  <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Rotten_Tomatoes_positive_audience.svg/212px-Rotten_Tomatoes_positive_audience.svg.png?20200101015220"
+                    alt=""
+                    className="h-5 w-5"
+                  />{" "}
+                  <span>{element.tomatoes.viewer.meter}%</span>
+                </div>
               </div>
-              <div className="grid md:grid-cols-2 sm:grid-cols-2">
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Rotten_Tomatoes_positive_audience.svg/212px-Rotten_Tomatoes_positive_audience.svg.png?20200101015220"
-                  alt=""
-                  className="h-5 w-5"
-                />{" "}
-                <span>{element.tomatoes.viewer.meter}%</span>
-              </div>
+              <h6 className="text-sm font-serif text-black">{element.title}</h6>
+              <p className="text-black text-sm">
+                Opened{" "}
+                {moment(element.tomatoes.lastUpdated).format("MMM Do YYYY")}
+              </p>
             </div>
-            <h6 className="text-sm font-serif text-black">{element.title}</h6>
-            <p className="text-black text-sm">
-              Opened{" "}
-              {moment(element.tomatoes.lastUpdated).format("MMM Do YYYY")}
-            </p>
-          </div>
-        ) : (
-          <div className="mx-auto px-1	">
-            <img
-              className="hover:opacity-75 rounded-lg h-96  shadow-2xl w-72 md:h-60"
-              src="https://www.take5reviews.ca/uploads/9/7/5/5/97557460/super-mario-bros-23_2.png"
-              alt=""
-            />
-          </div>
-        )
-      )}
-
+          ) : (
+            <div className="mx-auto px-1	">
+              <img
+                className="hover:opacity-75 rounded-lg h-96  shadow-2xl w-72 md:h-60"
+                src="https://www.take5reviews.ca/uploads/9/7/5/5/97557460/super-mario-bros-23_2.png"
+                alt=""
+              />
+            </div>
+          )
+        )}
+      </div>
       <Pagination setMovie={setMovie} />
     </div>
   );
