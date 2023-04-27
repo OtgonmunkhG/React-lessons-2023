918026f15adf35c8db2cee6b6b2a1f4b44764560
OtgonmunkhG <g.otgonmunkh@gmail.com>
2023-04-27 11:51:22 +0800

    added day-103 login with facebook oauth

--- day-102-login-o-auth/src/components/Header.tsx
+++ day-102-login-o-auth/src/components/Header.tsx
@@ -1,22 +1,58 @@
-import { useSession, signIn } from "next-auth/react";
-
+import { useSession, signIn, signOut } from "next-auth/react";
+import styles from "../styles/header.module.css";
 export default function Header() {
   const { data: session, status } = useSession();
+  const loading = status === "loading";
 
   const handleLogin = (e: any) => {
     e.preventDefault();
     signIn();
   };
+
+  const handleLogout = (e: any) => {
+    e.preventDefault();
+    signOut();
+  };
   return (
     <div>
       <header>
+        <noscript>
+          <style>{`.nojs-show {opacity:1 ; top: 0;}`}</style>
+        </noscript>
         <div>
-          <div>
+          <div className={styles.signedInStatus}>
             {!session && (
-              <div>
-                <span>You are not signed in</span>
+              <div
+                className={`.nojs-show ${
+                  !session && loading ? styles.loading : styles.loaded
+                }`}
+              >
+                <p>You are not signed in</p>
                 <a href={`/api/auth/signin`} onClick={handleLogin}>
                   Sign in
+                </a>
+              </div>
+            )}
+
+            {session?.user && (
+              <div>
+                {session?.user.image && (
+                  <span
+                    style={{ backgroundImage: `url(${session.user.image})` }}
+                    className={styles.avatar}
+                  ></span>
+                )}
+                <span className={styles.signedInText}>
+                  <small>Signed in as</small>
+                  <br />
+                  <strong>{session.user.email || session.user.name}</strong>
+                </span>
+                <a
+                  href={`/api/auth/signout`}
+                  className={styles.button}
+                  onClick={handleLogout}
+                >
+                  Sing out
                 </a>
               </div>
             )}
--- day-102-login-o-auth/src/pages/api/auth/[...nextauth].ts
+++ day-102-login-o-auth/src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,5 @@
 import GithubProdiver from "next-auth/providers/github";
+import FacebookProdiver from "next-auth/providers/facebook";
 import NextAuth, { NextAuthOptions } from "next-auth";
 
 export const authOptions: NextAuthOptions = {
@@ -6,6 +7,10 @@ export const authOptions: NextAuthOptions = {
     GithubProdiver({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+    }),
+    FacebookProdiver({
+      clientId: process.env.FACEBOOK_ID,
+      clientSecret: process.env.FACEBOOK_SECRET,
     }),
   ],
 };
